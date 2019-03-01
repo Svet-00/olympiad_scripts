d82d314950a8ef81e179751e32e28ceb87cbcefb
Svetozar <swetozar.volkov@yandex.ru>
2019-03-01 21:29:16 +0300

    maket: creation of TrueType text instructions added

--- maket/image.php
+++ maket/image.php
@@ -19,8 +19,23 @@ $bg = imagecolorallocate($img, 255, 255, 255);
 //рисование линии
 imageline($img, x1, y1, x2, y2, color);
 
+// Текст, используя шрифт TrueType - НУЖЕН ФАЙЛ СО ШРИФТОМ
+//х,у - координаты левого нижнего угла
+imagettftext($img, size, angle, x, y, color, font, text);
+
 //вывод изображения в стандартный вывод (браузер) или в файл
 imagepng($img, file_null);
 
 //освобождения памяти, выделенной под изображение
 imagedestroy($img);
+
+//Возвращаем цвет пикселя с координатами (10, 15) на изображении $image
+$color = imagecolorat($image, 10, 15);
+//Получаем составляющие цвета (red, green, blue)
+$r = ($color >> 16) & 0xFF;
+$g = ($color >> 8) & 0xFF;
+$b = $color & 0xFF;
+//Выводим результат
+echo $r."<br />";
+echo $g."<br />";
+echo $b."<br />";
